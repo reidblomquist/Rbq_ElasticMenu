56dc2f8e76f6d28f935f957cfcc48c2f9385e606
Reid Blomquist <reid@theprogrampdx.com>
2014-11-29 14:19:22 -0800

    adding elastica as submodule

--- lib/Elastica/Facet/Query.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Elastica_Facet_Query extends Elastica_Facet_Abstract
-{
-	public function toArray() {
-		$this->_setFacetParam('query', $this->_params);
-		return parent::toArray();
-	}
-}
